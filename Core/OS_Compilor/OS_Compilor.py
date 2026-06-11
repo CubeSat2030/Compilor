c0af4c0998573278d39899b1f1bc0d1cc1c73500
Nathan-Busse <bussenathan@icloud.com>
2026-06-11 16:52:06 +0200

    CubeSat is Alive!

--- Core/OS_Compilor/OS_Compilor.py
+++ Core/OS_Compilor/OS_Compilor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f6ea547f2d2a59a45e8acce8a68d8fc43f0a1490d07bf211094c084276522ea
-size 43979
+oid sha256:d497c5870cff1595d41a8e5a11947d8cb4df00f3f0cca0ccfbf613815fc082ab
+size 44144
