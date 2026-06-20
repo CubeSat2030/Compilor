e45b6da6abcbc38d495c87caa35b0b4bed588961
Nathan-Busse <bussenathan@icloud.com>
2026-06-20 12:47:35 +0200

    Added checksum compiler.

--- Core/OS_Compilor/OS_Compilor.py
+++ Core/OS_Compilor/OS_Compilor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6897c71c628fd147376f5707cf064145405114eef1dba26afa8833faf76cad73
-size 45967
+oid sha256:b7f90afdf2f1aabf7d6f07aa829354e640d88a152a44e6f91d258ac1457e67f1
+size 48293
