4719102612642d599b81821754252a5cfb1d1c85
Nathan-Busse <bussenathan@icloud.com>
2026-07-10 14:51:09 +0200

    Added build config to remember choices selscted on the last run.

--- Core/OS_Compilor/OS_Compilor.py
+++ Core/OS_Compilor/OS_Compilor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55b723bddbd3ae56b9fd41d4f70d887b3be3f98afb7bc2219872f7e1cb22d31b
-size 49918
+oid sha256:46684437ef67a431a5f9fd570950c6028db3e0ba31d4f16a9abc0c6546a729ef
+size 53449
