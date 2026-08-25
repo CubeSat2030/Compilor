0e34cc021eacc2a14dfe8f3af21177fb37f29547
Nathan-Busse <bussenathan@icloud.com>
2026-08-25 21:56:46 +0200

    Update file version and size in OS_Compilor.py

--- Core/app/components/OS_Compilor/OS_Compilor.py
+++ Core/app/components/OS_Compilor/OS_Compilor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c0c97a6b3a9285e5e2dfb853a54dc54c423f1e3a71b73cf39a661fce2f8f888
-size 109359
+oid sha256:e141478847e012eb811e4965c1e33364993ff82bf9bb8fbbad8c29c224a1d486
+size 120721
